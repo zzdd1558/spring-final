c6d0db17f5edaaec31a2fc4e6d7745a1fe56e01a
LDH <tigerho2@naver.com>
2017-11-10 17:27:50 +0900

    화장품브랜드DTO

---
+++ spring-final/src/main/java/com/thebeauty/model/domain/CosmeticBrandDTO.java
@@ -0,0 +1,55 @@
+package com.thebeauty.model.domain;
+
+
+/**
+ * @author 임대호
+ * @생성일 : 2017-11-10
+ * TABLE - COSMETIC_BRAND_TB
+ * 	: 화장품 브랜드 테이블
+ * 
+ * COLUMN :
+ *  - BRANDTYPE_IDX : 브랜드 코드
+ *  - BRANDTYPE_NAME : 브랜드명
+ *
+ */
+public class CosmeticBrandDTO {
+	private int brandTypeIdx; //BRANDTYPE_IDX
+	private String brandTypeName; //BRANDTYPE_NAME
+	
+	/* 생성자 */
+	public CosmeticBrandDTO() {}
+
+	public CosmeticBrandDTO(int brandTypeIdx, String brandTypeName) {
+		super();
+		this.brandTypeIdx = brandTypeIdx;
+		this.brandTypeName = brandTypeName;
+	}
+
+	/* getter setter*/
+	public int brandTypeIdx() {
+		return brandTypeIdx;
+	}
+
+	public void brandTypeIdx(int brandTypeIdx) {
+		this.brandTypeIdx = brandTypeIdx;
+	}
+
+	public String getBrandTypeName() {
+		return brandTypeName;
+	}
+
+	public void setBrandTypeName(String brandTypeName) {
+		this.brandTypeName = brandTypeName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("CosmeticBrandDTO [brandTypeIdx=");
+		builder.append(brandTypeIdx);
+		builder.append(", brandTypeName=");
+		builder.append(brandTypeName);
+		builder.append("]");
+		return builder.toString();
+	}
+}
