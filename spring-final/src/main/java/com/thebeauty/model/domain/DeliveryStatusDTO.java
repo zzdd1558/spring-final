1a8e77e208fb1fdbdff6964fcd57ab849a1b5042
LDH <tigerho2@naver.com>
2017-11-10 17:26:30 +0900

    배송상태DTO

---
+++ spring-final/src/main/java/com/thebeauty/model/domain/DeliveryStatusDTO.java
@@ -0,0 +1,56 @@
+package com.thebeauty.model.domain;
+
+
+/**
+ * @author 임대호
+ * @생성일 : 2017-11-10
+ * 
+ * TABLE : DELIVERY_STATUS_TB
+ * 	- 배송 정보 테이블
+ * 
+ * COLUMN : 
+ *  - DELI_STATUS_CODE : 배송타입
+ *  - DELI_STATUS_NAME : 배송상태
+ */
+public class DeliveryStatusDTO {
+	private int deliStatusCode; // DELI_STATUS_CODE
+	private String deliStatusName; // DELI_STATUS_NAME
+	
+	/* 생성자 */
+	public DeliveryStatusDTO() {}
+
+	public DeliveryStatusDTO(int deliStatusCode, String deliStatusName) {
+		super();
+		this.deliStatusCode = deliStatusCode;
+		this.deliStatusName = deliStatusName;
+	}
+
+	/*getter setter*/
+	public int getDeliStatusCode() {
+		return deliStatusCode;
+	}
+
+	public void setDeliStatusCode(int deliStatusCode) {
+		this.deliStatusCode = deliStatusCode;
+	}
+
+	public String getDeliStatusName() {
+		return deliStatusName;
+	}
+
+	public void setDeliStatusName(String deliStatusName) {
+		this.deliStatusName = deliStatusName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("DeliveryStatusDTO [deliStatusCode=");
+		builder.append(deliStatusCode);
+		builder.append(", deliStatusName=");
+		builder.append(deliStatusName);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
