d3bd8ed9aa046d7b5746e33f457fd87780da1d1e
Choi, Yun jin <zzdd1558@naver.com>
2017-11-11 21:02:28 +0900

    회원가입 Javascript 처리할 파일.
    숫자만 입력받는 리터럴 변수명 변경 및 phoneNumber와 birth부분 숫자처리하기 위해 공통로직으로 수정

--- spring-final/src/main/webapp/javascripts/signup.js
+++ spring-final/src/main/webapp/javascripts/signup.js
@@ -10,97 +10,10 @@ let uName = document.getElementById('u_name');
 let uPhoneNumber = document.getElementById('u_phone_number');
 let uEmail = document.getElementById('u_email');
 let uAddress = document.getElementById('u_address');
-let uSelectYear = document.getElementById('u_selectYear');
-let uSelectMonth = document.getElementById('u_selectMonth');
-let uSelectDate = document.getElementById('u_selectDate');
+let uBirthFront = document.getElementById('u_birth_front');
+let uBirthGender = document.getElementById('u_birth_gender');
 
 
-/* 현재 년도 - this.maxYear 한만큼 select option 생성. */
-function setYear() {
-    let result = "";
-    let checkSelect = "";
-    for (let i = dateFormat.getCurrentYear(); i > dateFormat.getCurrentYear() - dateFormat.getMaxYear(); i--) {
-
-        // 처음 기본 값을 현재 년도로 지정
-        if (i === dateFormat.getCurrentYear()) {
-            checkSelect = "selected='selected'";
-        }
-
-        // select > option 추가.
-        result += `<option ${checkSelect} value=${i}>${i}년</option>`;
-        checkSelect = '';
-    }
-    uSelectYear.innerHTML = result;
-}
-
-/* select option 에 1~12까지 세팅 */
-function setMonth() {
-    let ii;
-    let checkSelect = "";
-    let result = ``;
-
-    for (let i = 1; i <= 12; i++) {
-        if (i === dateFormat.getCurrentMonth()) {
-            console.log(i);
-            checkSelect = "selected='selected'";
-        }
-        if (i < 10) {
-            ii = "0" + i;
-        } else {
-            ii = i;
-        }
-        result += `<option ${checkSelect} value=${ii}>${i}월</option>`;
-        checkSelect = '';
-    }
-    uSelectMonth.innerHTML = result;
-}
-
-/*
- month가 바뀔때마다 select option 갱신.
- 매개변수 month가 없을경우 this.month로 대체.
- */
-function setDays(year = dateFormat.getCurrentYear(), month = dateFormat.getCurrentMonth()) {
-    console.log(` year : ${year}    ,     month : ${month}`);
-    let days = dateFormat.getFullDays(year, month);
-    console.log(days);
-    let ii;
-    let checkSelect = '';
-    let result = ``;
-    for (let i = 1; i <= days; i++) {
-
-        if (i === dateFormat.getCurrentDate()) {
-            checkSelect = "selected='selected'";
-        }
-        if (i < 10) {
-            ii = "0" + i;
-        } else {
-            ii = i;
-        }
-        result += `<option ${checkSelect} value=${ii}>${i}일</option>`;
-        checkSelect = '';
-    }
-
-    uSelectDate.innerHTML = result;
-}
-
-/* 월 selectBox에 change Event가 발생할 경우 일수 변경 (2월 윤년에 다른 요일 때문) */
-uSelectMonth.addEventListener("change", function () {
-    let changeYear = uSelectYear.options[uSelectYear.selectedIndex].value;
-    let changeMonth = uSelectMonth.options[uSelectMonth.selectedIndex].value;
-    if (changeMonth.charAt(0) == '0') {
-        changeMonth = changeMonth.charAt(1);
-    }
-    uSelectDate.innerHTML = "";
-    setDays(Number(changeYear), Number(changeMonth));
-});
-
-/* 년 selectBox에 change Event가 발생할 경우 일수 변경 (2월 윤년에 다른 요일 때문) */
-uSelectYear.addEventListener('change', function () {
-    let changeYear = uSelectYear.options[uSelectYear.selectedIndex].value;
-    let changeMonth = uSelectMonth.options[uSelectMonth.selectedIndex].value;
-    uSelectDate.innerHTML = "";
-    setDays(Number(changeYear), Number(changeMonth));
-});
 
 /* id 입력 keyUp이벤트 */
 uId.addEventListener("keyup", function () {
@@ -124,9 +37,17 @@ uEmail.addEventListener("keyup", function () {
 
 /* phone_number 입력 keyUp이벤트 */
 uPhoneNumber.addEventListener("keyup", function () {
-    inputKeyUpReg.keyUpPhone(uPhoneNumber.value);
+    inputKeyUpReg.keyUpOnlyNumber(uPhoneNumber, uPhoneNumber.value);
+});
+/* birth_front 입력 keyUp이벤트 */
+uBirthFront.addEventListener("keyup", function () {
+    inputKeyUpReg.keyUpOnlyNumber(uBirthFront,uBirthFront.value);
 });
 
+/* birth_gender 입력 keyUp이벤트 */
+uBirthGender.addEventListener("keyup", function () {
+    inputKeyUpReg.keyUpOnlyNumber(uBirthGender,uBirthGender.value);
+});
 
 /* userId 비동기 아이디 중복 체크 */
 uId.addEventListener("blur" , function(){
@@ -167,8 +88,3 @@ uPasswordConfirm.addEventListener("blur" , function(){
     }
 });
 
-
-
-setYear();
-setMonth();
-setDays();
