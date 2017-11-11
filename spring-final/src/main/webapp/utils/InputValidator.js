977bb80f72b485e5fa1a865de05a3a3b5d8dfb14
Choi, Yun jin <zzdd1558@naver.com>
2017-11-11 21:05:13 +0900

    변경

--- spring-final/src/main/webapp/utils/InputValidator.js
+++ spring-final/src/main/webapp/utils/InputValidator.js
@@ -7,11 +7,11 @@
 class InputValidator {
 
     constructor() {
-        /* 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다. */
-        this.idReg = /^[a-zA-Z]([a-zA-Z0-9_-]){4,19}$/;
+        /* 6~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다. */
+        this.idReg = /^[a-zA-Z]([a-zA-Z0-9_-]){5,19}$/;
 
-        /* 숫자 + 알파벳 + 특수문자를 포함한 6~16자리의 비밀번호 정규표현식*/
-        this.passwordReg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{6,16}/;
+        /* 숫자 + 알파벳 + 특수문자를 포함한 8~16자리의 비밀번호 정규표현식*/
+        this.passwordReg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}/;
 
         /* 이메일 정규 표현식*/
         this.emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -22,22 +22,22 @@ class InputValidator {
 
     /*  form 전송 전 id 유효성 검사*/
     isValidId(id) {
-        return this.isValidate(id, validKindsOfReg.idReg);
+        return this.isValidate(id, this.idReg);
     }
 
     /*   password 유효성 검사*/
     isValidPassword(password) {
-        return this.isValidate(password, validKindsOfReg.passwordReg);
+        return this.isValidate(password, this.passwordReg);
     }
 
     /*  form 전송 전 email 유효성 검사*/
     isValidEmail(email) {
-        return this.isValidate(email, validKindsOfReg.emailReg);
+        return this.isValidate(email, this.emailReg);
     }
 
     /*  form 전송 전 휴대폰번호 유효성 검사*/
-    isValidPhone(phone) {
-        return this.isValidate(phone, validKindsOfReg.phoneReg);
+    isValidPhone(phone) { 
+        return this.isValidate(phone, this.phoneReg);
     }
 
     isValidPasswordSame(password, passwordConfirm) {
