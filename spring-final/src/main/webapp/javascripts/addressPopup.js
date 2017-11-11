a992c2f911c02015d16ca024e414748311389e15
Choi, Yun jin <zzdd1558@naver.com>
2017-11-11 21:03:58 +0900

    우편주소 찾기 기능에서 callback으로 설정할 변수 추가

--- spring-final/src/main/webapp/javascripts/addressPopup.js
+++ spring-final/src/main/webapp/javascripts/addressPopup.js
@@ -12,8 +12,7 @@ function jusoCallBack(roadFullAddr, roadAddrPart1, addrDetail, roadAddrPart2,
 		bdKdcd, siNm, sggNm, emdNm, liNm, rn, udrtYn, buldMnnm, buldSlno, mtYn,
 		lnbrMnnm, lnbrSlno, emdNo) {
 	// 2017년 2월 제공항목이 추가되었습니다. 원하시는 항목을 추가하여 사용하시면 됩니다.
-	let form = document.getElementById('signupForm');
-	form.u_address_zip_no.value = zipNo;
-	form.u_address_part.value = roadAddrPart1;
-	form.u_address_detail.value = addrDetail;
+	document.getElementById('u_address_zip_no').value = zipNo;
+	document.getElementById('u_address_part').value = roadAddrPart1;
+	document.getElementById('u_address_detail').value = bdNm + " " +addrDetail;
 }
