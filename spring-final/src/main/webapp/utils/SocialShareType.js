66f76a3fb5e9aed036c9a676b54ff1cea22c4a1e
Choi, Yun jin <zzdd1558@naver.com>
2017-11-11 21:01:51 +0900

    소셜 타입별 공유하기 Javascript 파일
    주석 추가

--- spring-final/src/main/webapp/utils/SocialShareType.js
+++ spring-final/src/main/webapp/utils/SocialShareType.js
@@ -2,10 +2,12 @@
  * Created by yunjin on 2017-11-07.
  */
 
-
+/* 소셜 타입별 공유하기 script 파일 */
 class SocialShareType{
 
     constructor(){
+    	
+    	/* switch에서 사용할 소셜 타입 리터럴 변수*/
         this.SOCIALTYPE = {
             KAKAO : 'kakao',
             FACEBOOK : `facebook`,
@@ -13,8 +15,10 @@ class SocialShareType{
             NAVER : `naver`,
         };
 
+        /* 현재 아이템의 url을 얻어오는 부분 원래는 location.href 이어야 하나 localhost를 지원하지 않아 임의로 작성 */
         this.href = 'https://www.naver.com';
 
+        /* social type 별 url을 저장한 리터럴 변수. */
         this.socialURL = {
             /* localhost.href로 하면 localhost라 지원 안함. */
             'kakao' : `https://story.kakao.com/s/share?url=${encodeURIComponent(this.href)}`,
@@ -29,6 +33,7 @@ class SocialShareType{
 
     shareURL(socialType){
 
+    	/* 소셜 타입별 공유하기 */
         switch (socialType){
             case this.SOCIALTYPE.KAKAO :
                 this.url = this.socialURL.kakao;
@@ -50,6 +55,8 @@ class SocialShareType{
                 this.title = '네이버 공유하기';
                 break;
         }
+        
+        /* 공유하기 창을 새로운창으로 */
         window.open(this.url , this.title , 'width=672, height=561, toolbar=no, menubar=no, scrollbars=no, resizable=yes');
     }
 }
