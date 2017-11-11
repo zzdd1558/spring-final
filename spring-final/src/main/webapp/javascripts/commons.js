3ad7376d68efdb9affce9b558fa75c3dc06780c3
Choi, Yun jin <zzdd1558@naver.com>
2017-11-11 17:17:58 +0900

    공통 css , javascript 관련 파일 내용 추가. ( sidebar 관련 css 및 함수 commons.css 및 commons.js로 이동)

--- spring-final/src/main/webapp/javascripts/commons.js
+++ spring-final/src/main/webapp/javascripts/commons.js
@@ -1,3 +1,16 @@
-/**
- * 
- sdfasfsfasd*/
+//------------------------SideBar script-----------------------
+	var flag=true;
+	function OnOffNav() {
+		//left: 50%; margin-left: -20px;
+		if(flag){
+		    document.getElementById("mySidenav").style.width = "8%";
+//	 	    document.getElementById("main").style.width = "92%";
+			document.getElementById("quickTop").style.marginLeft ="2.7%";
+			flag=false;
+		}else{
+			document.getElementById("mySidenav").style.width = "40px";
+//	 	    document.getElementById("main").style.width= "96%";
+			document.getElementById("quickTop").style.marginLeft ="0px";
+		    flag=true;
+		}
+	}
