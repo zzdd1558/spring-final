23860a417e37ccfdb6320f06e36cff997abb8109
Choi, Yun jin <zzdd1558@naver.com>
2017-11-13 18:18:48 +0900

    id와 password를 비교할 spring-security provider 파일 생성

---
+++ spring-final/src/main/java/com/thebeauty/security/provider/UserAuthenticationProvider.java
@@ -0,0 +1,21 @@
+package com.thebeauty.security.provider;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class UserAuthenticationProvider implements AuthenticationProvider{
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		// TODO Auto-generated method stub
+		return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+	}
+
+}
