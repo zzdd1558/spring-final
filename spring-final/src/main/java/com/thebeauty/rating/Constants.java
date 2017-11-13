d4fa960260367872b3e3e77513a6a614601fab1f
Choi, Yun jin <zzdd1558@naver.com>
2017-11-13 15:28:57 +0900

    각 회원 등급을 나눠논 interface

---
+++ spring-final/src/main/java/com/thebeauty/rating/Constants.java
@@ -0,0 +1,26 @@
+package com.thebeauty.rating;
+
+public class Constants {
+	
+	/* Admin 권한 */
+	public static String ADMIN = "A";
+	
+	/* 회원 승인 대기 권한 */
+	public static String memberWaiting = "N";
+	
+	/* 브론즈 권한 */
+	public static String bronze = "B";
+	
+	/* 실버 권한 */
+	public static String silver = "S";
+	
+	/* 골드 권한 */
+	public static String gold = "G";
+	
+	/* 플레티넘 권한 */
+	public static String platinum = "P";
+	
+	/* 다이아몬드 권한 */
+	public static String diamonde = "D";
+	
+}
