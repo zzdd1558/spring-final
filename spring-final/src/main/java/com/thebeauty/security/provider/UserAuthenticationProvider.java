676ad18809424549b62970e314b6a38fa3269053
Choi, Yun jin <zzdd1558@naver.com>
2017-11-13 20:24:44 +0900

    UserAuthenticationProvider spring-security 설정 추가

--- spring-final/src/main/java/com/thebeauty/security/provider/UserAuthenticationProvider.java
+++ spring-final/src/main/java/com/thebeauty/security/provider/UserAuthenticationProvider.java
@@ -4,12 +4,14 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
 
+@Service(value="userAuthenticationProvider")
 public class UserAuthenticationProvider implements AuthenticationProvider{
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		// TODO Auto-generated method stub
-		return null;
+		return new UsernamePasswordAuthenticationToken(null, null, null);
 	}
 
 	@Override
