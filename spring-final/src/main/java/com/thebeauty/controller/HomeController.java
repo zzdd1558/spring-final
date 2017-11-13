06d2e37f2c5c7d2539615904d139780c9d5e7c99
Choi, Yun jin <zzdd1558@naver.com>
2017-11-13 15:22:28 +0900

    spring security 테스트용 파일

--- spring-final/src/main/java/com/thebeauty/controller/HomeController.java
+++ spring-final/src/main/java/com/thebeauty/controller/HomeController.java
@@ -1,30 +1,41 @@
 package com.thebeauty.controller;
 
+import java.util.List;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.thebeauty.model.dao.Test;
+import com.thebeauty.model.domain.FaceTypeDTO;
+
 @Controller
 public class HomeController {
-// main start
+
+	@Autowired
+	private Test test;
+
+	// main start
 	@RequestMapping(value = "openmallMain.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		return "main";   
+		return "main";
 	}
-	
+
 	/* 회원가입 페이지 이동 */
-	@RequestMapping(value = "userJoin.do" , method = RequestMethod.GET)
+	@RequestMapping(value = "userJoin.do", method = RequestMethod.GET)
 	public String userJoin() {
 		System.out.println("회원가입 페이지  호출");
+		test.test();
 		return "userJoin";
 	}
+
 	@RequestMapping(value = "addressSearch.do")
 	public String addressSearch() {
 		System.out.println("addressSearch 호출");
 		return "addressSearch";
 	}
-	
+
 }
