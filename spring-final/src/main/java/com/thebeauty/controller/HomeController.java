2215dcd71a3f0d2113dfb5b373bbd19a16c085e5
Choi, Yun jin <zzdd1558@naver.com>
2017-11-21 14:43:12 +0900

    단순 경로이동 삭제 후 jsp로만 이동

--- spring-final/src/main/java/com/thebeauty/controller/HomeController.java
+++ spring-final/src/main/java/com/thebeauty/controller/HomeController.java
@@ -10,30 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	/* main page start*/
+	/* 메인페이지 시작 */
 	@RequestMapping(value = "openmallMain.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		return "main";
 	}
-
-
-	/* 회원가입 페이지 이동 */
-	@RequestMapping(value = "userJoin.do", method = RequestMethod.GET)
-	public String userJoin() {
-		return "userJoin";
-	}
-
-	
 	/* 우편주소 찾기위해 사용하는 URL*/
 	@RequestMapping(value = "addressSearch.do")
 	public String addressSearch() {
 		return "addressSearch";
-	}
-
-	/* 회원로그인 페이지 이동*/
-	@RequestMapping(value = "userLogin.do" , method = RequestMethod.GET)
-	public String userLogin() {
-		return "userLogin";
 	}
 	
 	/*==============           게시판 form 단순 경로            ============== */
@@ -41,10 +26,5 @@ public class HomeController {
 	@RequestMapping(value = "board/BoardWriteForm.do", method = RequestMethod.GET)
 	public String boardWriteForm() {
 		return "board/boardWrite";
-	}
-	/* 아디찾기 페이지 이동*/
-	@RequestMapping("idSearch.do")
-	public String idSearch() {
-		return "idSearch";
 	}
 }
